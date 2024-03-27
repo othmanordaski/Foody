1976fecd13edd94a850100d76dec13c3f6f559ac
othmane errouissat <rouissatothman@gmail.com>
2024-03-27 22:39:59 +0000

    Setup initial server configuration and essential dependencies

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-const express = require('express');
-require('dotenv').config();
+require('dotenv').config()
+const {server} = require('./config/config');
+const PORT = server.PORT
+const express = require('express')
+const app = express()
+const cookieParser = require('cookie-parser')
 
-const app = express();
-const port = process.env.PORT || 3000; 
+//Import the database connection function 
+const {connection} = require('./config/database')
+const database = connection()
+
+//Middleware setup
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+//Connect to the MongoDb databse
+database.connectToMongo()
+
+const userRoutes = require('./Routes/UserRoute')
+const {default : mongoose} = require('mongoose')
 
 
-app.use(express.json());
-
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-
-app.listen(port, () => {
-  console.log('Server is running on http://localhost:'+ PORT);
+// Start the Express server and listen on port 3000
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
 });
