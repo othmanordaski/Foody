6a02fd1196deaa31a5f35f45f2cef0e98511140b
SaraTahiri <tahirisara911@gmail.com>
2024-03-28 12:38:49 +0000

    Update and delete profile

--- Controllers/Profile.Controllers.js
+++ Controllers/Profile.Controllers.js
@@ -14,11 +14,65 @@ exports.profileUser = async (req,res) => {
     }
 }
 
+exports.UpdateProfile = async (req, res) => {
+    try {
+        const id = req.user.user._id;
+        const { username, email, password, age, country, sex, phoneNumber, bio } = req.body;
+
+        const userfound = await User.findById(id);
+        if (!userfound) {
+            return res.status(404).json({ message: 'User not found.' });
+        }
+
+        if (password) {
+            const check = await comparePassword(password, userfound.password);
+            if (check) {
+                return res.json({ message: 'Same password' });
+            }
+        }
+
+        let hashedPassword;
+        if (password) {
+            hashedPassword = await hashPassword(password);
+        }
+
+        const updateFields = {
+            username,
+            email,
+            age,
+            country,
+            sex,
+            phoneNumber,
+            bio
+        };
+
+        if (hashedPassword) {
+            updateFields.password = hashedPassword;
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(id, updateFields, { new: true });
+
+        res.json({
+            message: 'Profile updated successfully',
+            data: updatedUser
+        });
+    } catch (error) {
+        console.error('Error updating profile:', error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+}
+/* ---------------------------------------------------------------------------------------------------------------
 exports.UpdateProfile = async (req,res) => {
     try{
         const id = req.user.user._id
-        const {username, email, password, age, country, sex, phoneNumber} = req.body;
+        //console.log(id)
+        const {username, email, password, age, country, sex, phoneNumber,bio} = req.body;
         const userfound = await User.findById(id)
+
+        if (!userfound) {
+            return res.status(404).json({ message: 'User not found.' });
+        }
+
         const check = await comparePassword(password,userfound.password)
         if(check){
             return res.json({message : 'Same password'})
@@ -27,8 +81,8 @@ exports.UpdateProfile = async (req,res) => {
             const user = await User.updateOne({_id : id},
                 { 
                         username,
-                         email, 
-                         password : hashedPassword,  
+                         email,
+                         password : hashedPassword , 
                          age, 
                          country, 
                          sex,
@@ -44,12 +98,15 @@ exports.UpdateProfile = async (req,res) => {
         res.status(500).send('Server Error');
     }
 }
+--------------------------------------------------------------------------------------------------------------
+*/
 
 exports.deleteProfile = async (req,res) => {
     try {
         const id= req.user.user._id
         const user = await User.deleteOne({_id : id})
         res.json({
+            message: 'Profile deleted successfully',
             data: user
         });
     }catch (err) {
