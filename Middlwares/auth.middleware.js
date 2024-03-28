9b4b0c7cba8d06e3efc298932c27792c7965379f
imane-h <imanehoujaa@gmail.com>
2024-03-28 14:54:04 +0100

    Modify Auth file

---
+++ Middlwares/auth.middleware.js
@@ -0,0 +1,23 @@
+const {verifyToken} = require('../Helpers/JWT'); // Assuming verifyToken function is imported correctly
+
+exports.isAuthenticated = (req, res, next) => {
+    try {
+        const token = req.cookies.tokenAuth || null;
+        //console.log(token)
+        if (!token) {
+            return res.status(401).json({ message: 'Empty token. Please login to access this resource.' });
+        }
+
+        const verify = verifyToken(token);
+
+        if (!verify) {
+            return res.status(401).json({ message: 'Session not found. Please login again.' });
+        }
+        console.log(verify)
+        req.user = verify;
+        next();
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
