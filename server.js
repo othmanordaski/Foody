8f5e15c0c9799b2288b330d59c6ead335c49291d
othmane errouissat <rouissatothman@gmail.com>
2024-03-28 14:14:53 +0000

    modify the server file

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
+
 const {server} = require('./config/config');
 const PORT = server.PORT
+
 const express = require('express')
 const app = express()
+
+const resturant = require('./Routes/RestaurantRoute')
 const cookieParser = require('cookie-parser')
 
 //Import the database connection function 
@@ -17,9 +21,9 @@ app.use(express.urlencoded({extended : true}))
 //Connect to the MongoDb databse
 database.connectToMongo()
 
-const userRoutes = require('./Routes/UserRoute')
 const {default : mongoose} = require('mongoose')
 
+app.use('/restaurant',resturant)
 
 // Start the Express server and listen on port 3000
 app.listen(PORT, () => {
