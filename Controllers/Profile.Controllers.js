7f334fb494a718b082bc787514d9a02fb9d462a2
SaraTahiri <tahirisara911@gmail.com>
2024-04-02 14:36:40 +0000

    View,Update and delete profile

--- Controllers/Profile.Controllers.js
+++ Controllers/Profile.Controllers.js
@@ -6,7 +6,17 @@ exports.profileUser = async (req,res) => {
         const id = req.user.user._id
         const user = await User.findById(id)
         if(!user) return res.status(404).json({message : 'User not found'})
-        res.status(200).json(user.username)
+
+        const fields = {
+            username : user.username,
+            email : user.email,
+            age : user.age,
+            country : user.country,
+            sex : user.sex,
+            phoneNumber : user.phoneNumber,
+            bio : user.bio
+        }
+        res.status(200).json(fields)
 
     }catch(error){
         res.status(500).send('Server Error');
@@ -54,7 +64,6 @@ exports.UpdateProfile = async (req, res) => {
 
         res.json({
             message: 'Profile updated successfully',
-            data: updatedUser.username
         });
     } catch (error) {
         res.status(500).json({ message: 'Server Error' });
@@ -102,7 +111,6 @@ exports.deleteProfile = async (req,res) => {
         const user = await User.deleteOne({_id : id})
         res.json({
             message: 'Profile deleted successfully',
-            data: user
         });
     }catch (err) {
         res.status(500).json({ message: 'Server error' });
