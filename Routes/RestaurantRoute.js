190e8bd911a8fa2131f429193bb6d8dd6bf68778
SaraTahiri <tahirisara911@gmail.com>
2024-04-23 22:32:12 +0100

    Add route for restaurant register, login, profile, update and delete

--- Routes/RestaurantRoute.js
+++ Routes/RestaurantRoute.js
@@ -2,16 +2,32 @@ const express = require('express')
 const router = express.Router()
 
 const uploadImage = require('../Middlwares/multer')
-const {getAllMenus, getSingleMenu, createMenu, updateMenu, deleteMenu}=require('../Controllers/restaurantController')
+const {getAllMenus, getSingleMenu, createMenu, updateMenu, deleteMenu}=require('../Controllers/MenuController')
 
+const {registerRestaurant,restaurantLogin,getRestaurantProfile,updateRestaurantProfile,deleteRestaurantProfile} = require('../Controllers/RestauControllers')
+const {isAuthenticated} = require('../Middlwares/auth.middleware')
 
+router.route('/register')
+.post(registerRestaurant)
+
+router.route('/login')
+.post(restaurantLogin)
+
+router.route('/profile')
+.get(isAuthenticated,getRestaurantProfile)
+
+router.route('/profile/edit/:id')
+.patch(updateRestaurantProfile)
+
+router.route('/profile/delete/:id')
+.delete(deleteRestaurantProfile)
 
 router.route('/menu')
 .get(getAllMenus)
-.post(uploadImage,createMenu)
+.post(isAuthenticated,uploadImage,createMenu)
 router.route('/menu/:id')
 .get(getSingleMenu)
-.patch(uploadImage,updateMenu)
-.delete(deleteMenu)
+.patch(isAuthenticated,uploadImage,updateMenu)
+.delete(isAuthenticated,deleteMenu)
 
 module.exports = router
